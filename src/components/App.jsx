22998bb195c5e5a5f56b8a11da36bf3f95b7f029
Qiuyu Chen <84677030+qc201@users.noreply.github.com>
2022-08-17 22:07:42 -0700

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prev) => {
+      return [...prev, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    console.log("clicked on delete");
+    console.log(id);
+    setNotes((prev) => {
+      return prev.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((note, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
